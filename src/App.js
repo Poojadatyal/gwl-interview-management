bbf689442b3c93cab3ebce2bfab324df90519e00
Pooja datyal <pooja.d@goodworks.com>
2019-08-22 12:45:05 +0530

    Menu created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './components/Menu';
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//import Menu from './Components/Menu';
 
+//import Login from './Components/Login';
+
+import Registeration_Frm from './Components/Registeration_Frm';
 function App() {
   return (
-    <div className="App">
-      <Menu />
-    </div>
+  /*  <Router>
+    <div>
+
+   </div>
+   <Route exact path="/"  component={Login} />
+   <Route exact path="/Menu"  component={Menu} />
+   </Router>*/
+
+   <Registeration_Frm/>
   );
 }
 
