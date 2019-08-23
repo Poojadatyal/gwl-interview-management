bad116c8f18a4f1d21fb14080886144730d6b4bc
Pooja datyal <pooja.d@goodworks.com>
2019-08-23 18:54:58 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-<<<<<<< HEAD
 import Menu from './components/Menu';
 import Login from './components/Login';
 import RegisterationForm from './components/Menu/RegisterationForm';
@@ -16,28 +15,7 @@ function App() {
    <Route exact path = "/Menu/RegisterationForm" component = {RegisterationForm} />
    </Router>
 </div>
-=======
-/*import Login from './Components/Login';
-import Menu from './Components/Menu';*/
-import Registeration_Frm from './Components/Registeration_Frm';
-import Login from './components/Login';
-import Menu from './components/Menu';
-
-function App() {
-  return (
-  /* <Router>
-    <div>
-<Registeration_Frm/>
-   </div>
-   <Route exact path="/"  component={Login} />
-   <Route exact path="/Menu"  component={Menu} />
-   </Router>*/
- <div>
-   <Menu/>
-  </div>
-
->>>>>>> 62f3bd80c52ee4fed4ce69497cde5758d0498c64
-  );
+);
 }
 
 export default App;
